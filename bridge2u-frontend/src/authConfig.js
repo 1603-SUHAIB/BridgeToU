4aa15ad8b3fb5e69e41d11f101ce4be4f948c080
moham <mohammedsuhaibv@gmail.com>
2025-08-11 20:27:00 +0530

    Deploy frontend

--- bridge2u-frontend/src/authConfig.js
+++ bridge2u-frontend/src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "34c184a5-3034-475a-a3f5-63cb6a9d090d",
+    clientId: "e96cf8fa-e90a-4629-ba2d-9e462da78e2b",
     authority: `https://login.microsoftonline.com/bcb7e647-9364-44c8-9543-3e67f39424b3`,
     redirectUri: "https://red-river-0d54dc60f.1.azurestaticapps.net",
   }
